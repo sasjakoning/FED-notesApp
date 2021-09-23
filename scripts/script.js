75d8de2155d9f409613e28fefc2c55aef5fbc861
sasja koning <sasjakoning@hotmail.com>
2021-09-23 19:49:37 +0200

    started working on the JS hell of randomly generating temps and moving items based on that

--- scripts/script.js
+++ scripts/script.js
@@ -57,3 +57,59 @@ console.log(randomElement)
 
     // random numbers: https://css-tricks.com/random-numbers-css/
 
+
+
+    const svgElement = document.querySelector(".forecast__temps")
+    const svgPath = document.getElementById("path_1")
+
+    const svgGroup3Dot = document.getElementById("group-3-dot")
+
+    console.log(svgElement, svgPath)
+
+
+    let number = 0;
+
+    // generate random temperature number
+    const randomTemperature = Math.floor(Math.random()*45)
+
+    function ding2(){
+        number = randomTemperature;
+
+        let textDing = document.getElementById("group-2-temp")
+        let root = document.getElementById("group_2")
+
+        if(randomTemperature < 18){
+            console.log("yes")
+            root.style.setProperty("--translateY","-" + number + "px")
+
+            textDing.textContent= number + "°";
+
+        }else{
+            console.log("no")
+            let root = document.getElementById("group_2")
+            root.style.setProperty("--translateY","-" + number + "px")
+
+            textDing.textContent= number + "°";
+        }
+    }
+
+    ding2()
+
+    console.log(randomTemperature)
+
+    function ding(){
+        number = 20;
+        let root = document.getElementById("group_1")
+        root.style.setProperty("--translateY", number + "px")
+
+        let textDing = document.getElementById("group-4-temp")
+
+        textDing.textContent="20" + "°";
+    }
+
+    // change css variable: https://css-tricks.com/updating-a-css-variable-with-javascript/
+
+    svgElement.addEventListener("click", function(){
+        svgPath.setAttribute("points", "12,45.7 97.9,25.7 183.8,25.7 269.7,25.7 355.6,25.7 441.5,25.7")
+        ding()
+    })
