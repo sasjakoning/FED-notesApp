74af9f6b5b3118a262e521638449af423806fabb
sasja koning <sasjakoning@hotmail.com>
2021-10-05 12:29:40 +0200

    finished first part of temperature js code

--- scripts/script.js
+++ scripts/script.js
@@ -1,133 +1,103 @@
 // JavaScript Document
 
-const container = document.querySelector(".sunAnim");
-let state = "fave";
+function Page() {
+  if (document.querySelector("body").classList.contains("forecast__page")) {
+    let pathNumber = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let plusOrMinus = ["", "", "", "", "", "", "", "", "", "", "", ""];
 
-var animation = bodymovin.loadAnimation({
-  container: document.querySelector(".sunAnim"),
-  renderer: "svg",
-  loop: false,
-  autoplay: false,
-  easing: "ease-out",
-  path: "scripts/sunrise-down.json",
-});
+    let tempGroup = document.querySelectorAll("#temps-group1 > g");
 
-animation.setSpeed(1.5);
+    console.log(tempGroup);
 
-const segments = [40, 10, 20, 61];
+    tempGroup.forEach((group, index) => {
+      const randomTemperature = Math.floor(Math.random() * 22);
+      number = randomTemperature * 2;
+      groupNumber = index + 1;
 
-var randomElement = segments[Math.floor(Math.random() * segments.length)];
-setTimeout(function () {
-  animation.playSegments([1, randomElement], true);
-}, 2000);
+      let tempText = document.querySelector("#group-" + groupNumber + "-temp");
+
+      console.log(tempText, index);
+
+      if (randomTemperature < 11) {
+        group.style.setProperty("--translateY", "-" + number + "px");
+
+        tempText.textContent = randomTemperature + "°";
+
+        pathNumber[index] = 22 + (11 - randomTemperature) * 2;
+      } else {
+        group.style.setProperty("--translateY", "-" + number + "px");
+
+        tempText.textContent = randomTemperature + "°";
+
+        pathNumber[index] = 22 + (11 - randomTemperature) * 2;
+      }
+    });
+
+    function pathPoints1(pointsList) {
+      let polyline = document.getElementById("path_1");
+
+      polyline.setAttribute("points", pointsList);
+    }
+
+    pathPoints1(
+      "12," +
+        pathNumber[0] +
+        " 97.9," +
+        pathNumber[1] +
+        " 183.8," +
+        pathNumber[2] +
+        " 269.7," +
+        pathNumber[3] +
+        " 355.6," +
+        pathNumber[4] +
+        " 441.5," +
+        pathNumber[5]
+    );
 
 
-// random wind speed direction
-
-const rotation = document.querySelectorAll(
-  ".mainContent__hourly__windSpeed > img "
-);
-
-rotation.forEach((element) => {
-  element.style.setProperty(
-    "--randomRotation",
-    Math.floor(Math.random() * 360) + "deg"
-  );
-});
-
-// random numbers: https://css-tricks.com/random-numbers-css/
 
 
-let pathNumber = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-let plusOrMinus = ["", "", "", "", "", "", "", "", "", "", "", ""];
 
-let tempGroup = document.querySelectorAll("#temps-group > g")
+  } else {
 
-console.log(tempGroup)
+    // sunrise/down animation
 
-tempGroup.forEach((group, index) => {
+    const container = document.querySelector(".sunAnim");
+    let state = "fave";
 
-  // console.log(group.firstChild.nextSibling, index)
+    const animation = bodymovin.loadAnimation({
+      container: document.querySelector(".sunAnim"),
+      renderer: "svg",
+      loop: false,
+      autoplay: false,
+      easing: "ease-out",
+      path: "scripts/sunrise-down.json",
+    });
 
-  const randomTemperature = Math.floor(Math.random() * 44);
-  number = randomTemperature * 2;
+    animation.setSpeed(1.5);
 
-  let tempText = document.querySelector("text")
+    const segments = [40, 10, 20, 61];
 
-  if(randomTemperature < 22) {
-    group.style.setProperty("--translateY", "-" + number + "px")
+    var randomElement = segments[Math.floor(Math.random() * segments.length)];
+    setTimeout(function () {
+      animation.playSegments([1, randomElement], true);
+    }, 2000);
 
-    tempText.textContent = randomTemperature + "°";
+    // random wind speed direction
 
-    pathNumber[index] = randomTemperature - 22;
-    pathNumber[index] = pathNumber[index] * 2;
-    pathNumber[index] = Math.abs(pathNumber[index] - 0);
+    const rotation = document.querySelectorAll(
+      ".mainContent__hourly__windSpeed > img "
+    );
 
-  }else {
-    group.style.setProperty("--translateY", "-" + number + "px")
-    
-    tempText.textContent = randomTemperature + "°";
+    rotation.forEach((element) => {
+      element.style.setProperty(
+        "--randomRotation",
+        Math.floor(Math.random() * 360) + "deg"
+      );
+    });
 
-    pathNumber[index] = randomTemperature - 22;
-    pathNumber[index] = pathNumber[index] * 2;
-
-    plusOrMinus[index] = "-";
+    // random numbers: https://css-tricks.com/random-numbers-css/
   }
-
-  console.log(randomTemperature, pathNumber[index])
-
-  // console.log(tempText.textContent, index)
-
-  // function pathPoints(pointsList) {
-  //   let polyline = document.getElementById("path_1");
-  
-  //   polyline.setAttribute("points", pointsList);
-  // }
-  
-  // pathPoints(
-  //   "12," +
-  //     plusOrMinus +
-  //     pathNumber +
-  //     " 97.9,0 183.8,0  269.7,0  355.6,0  441.5,0"
-  // );
-
-  // switch(index) {
-  //   case 0 :
-  //     console.log("hello")
-  //     break;
-  //   case 1 :
-  //     console.log("hello")
-  //     break;
-  //   case 2 :
-  //     console.log("hello")
-  //     break;
-  //   default:
-  //     console.log("doesnt work")
-  // }
-
-})
-
-
-function pathPoints(pointsList) {
-  let polyline = document.getElementById("path_1");
-
-  polyline.setAttribute("points", pointsList);
 }
 
-pathPoints(
-  "12," +
-    plusOrMinus[0] +
-    pathNumber[0] +
-    " 97.9," + plusOrMinus[1] +
-    pathNumber[1] +
-    " 183.8," + plusOrMinus[2] +
-    pathNumber[2] +
-    " 269.7," + plusOrMinus[3] +
-    pathNumber[3] +
-    " 355.6," + plusOrMinus[4] +
-    pathNumber[4] +
-    " 441.5," + plusOrMinus[5] +
-    pathNumber[5]
-);
-
-console.log(plusOrMinus, pathNumber)
+Page();
